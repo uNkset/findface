a59900a95987a66d4a31a8f78ec854fd0ce0dffb
Konstantin Arescenko <kostylo@windowslive.com>
2023-02-14 17:12:10 +0100

    navigation skeleton done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navigation from './components/Navigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Navigation />
-        <Logo />
+        <Navigation />
+        {/*<Logo />
         <ImageLinkForm />
         <FaceRecognition /> */}
       </div>
